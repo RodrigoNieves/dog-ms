0a35619dce9b829c5ec21e927e620f6a31eb67e7
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-20 20:18:39 -0800

    Initialize Dog Microservice with Config Client and Eureka Server

---
+++ src/main/java/me/rrsn/dogms/DogMsApplication.java
@@ -0,0 +1,13 @@
+package me.rrsn.dogms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/rrsn/dogms/DogMsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rrsn.dogms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
