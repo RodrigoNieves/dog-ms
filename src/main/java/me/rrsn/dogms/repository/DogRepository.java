71e423bed1186d153dbaf0d7272a318254ff11ad
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-11-23 16:55:22 -0800

    Rollback

--- src/main/java/me/rrsn/dogms/repository/DogRepository.java
+++
@@ -1,8 +0,0 @@
-package me.rrsn.dogms.repository;
-
-
-import me.rrsn.dogms.entity.Dog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DogRepository extends CrudRepository<Dog,Long> {
-}
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Firulais', 'Salchicha', 'Germany');
-INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Doggy', 'San bernado', 'USA');
